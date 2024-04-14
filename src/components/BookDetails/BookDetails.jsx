6b6b3e5789eaa671cded21aca2590f455c46186a
Abbby3 <ihfmx3@outlook.com>
2024-04-15 05:28:39 +1000

    added details modal

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,24 +1,33 @@
-import React from "react";
+import linkImg from "../../assets/link.png";
 import styles from "./BookDetails.module.scss";
 
 const BookDetails = ({ selectedBook, setSelectedBook }) => {
   const handleClose = () => {
     setSelectedBook(null);
   };
-  console.log(selectedBook);
 
   return (
     selectedBook && (
       <div className={styles.modal}>
-        <button onClick={handleClose}>Close</button>
-        <h2>{selectedBook.title}</h2>
+        <div className={styles.head}>
+          <h2>{selectedBook.title}</h2>
+          <button className={styles.btn} onClick={handleClose}>
+            Close
+          </button>
+        </div>
         <p>Author(s): {selectedBook.authors.join(", ")}</p>
         <p>
-          Publisher: {selectedBook.publisher}, published {selectedBook.publishedDate}
+          Publisher: {selectedBook.publisher ? selectedBook.publisher : "N/A"}
+          {selectedBook.publishedDate && ", published " + selectedBook.publishedDate}
         </p>
         <p>{selectedBook.description}</p>
         {selectedBook.saleLink ? (
-          <a href={selectedBook.saleLink} target="_blank" rel="noopener noreferrer">
+          <a
+            className={styles.link}
+            href={selectedBook.saleLink}
+            target="_blank"
+          >
+            <img className={styles.img} src={linkImg} alt="external link" />
             More Information
           </a>
         ) : (
